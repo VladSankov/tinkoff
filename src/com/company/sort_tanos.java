bb5b819e537a1d5d558e5b0d0c33392b6d05c1cc
vlados1488 <itunse169@mail.ru>
2021-02-17 23:35:16 +0300

    sort_tanos

--- src/com/company/sort_tanos.java
+++ src/com/company/sort_tanos.java
@@ -7,7 +7,7 @@ public class sort_tanos {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Input a number: ");
+        System.out.print("Input a number: dhdhdh ");
         String s = in.nextLine();
 
     }
